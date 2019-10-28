3599a4c6bfdf08338a9a6dce479be55b181b9311
Mehul Chopada <chopada@de3mcl-527941.ttg.global>
2019-10-28 17:24:32 +0100

    Add new files

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,17 +1,50 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
-const HomeScreen = ({ navigation }) => {
+const HomeScreen = ({navigation}) => {
   return (
     <View>
-      <Text style={{ fontSize: 30 }}>HomeScreen</Text>
-      <Button title="Layout" onPress={() => navigation.navigate('Layout')} />
-    </View>
-  );
+  <Text style={styles.text}>Welcome to react native</Text>
+    <Button 
+    title='Go to Component Demo' 
+    onPress={()=>navigation.navigate('Components')}
+    />
+       <Button 
+    title='Go to List Demo' 
+    onPress={()=>navigation.navigate('List')}
+    />
+      <Button 
+    title='Go to Image Demo' 
+    onPress={()=>navigation.navigate('Image')}
+    />
+    <Button 
+    title='Go to Counter Screen' 
+    onPress={()=>navigation.navigate('Counter')}
+    />
+    <Button 
+    title='Go to Color Screen' 
+    onPress={()=>navigation.navigate('Color')}
+    />
+    <Button 
+    title='Go to Square Screen' 
+    onPress={()=>navigation.navigate('Sqaure')}
+    />
+    <Button 
+    title='Go to Square Screen With Reducer' 
+    onPress={()=>navigation.navigate('SqaureWithReducer')}
+    />
+        <Button 
+    title='Go to Text Screen' 
+    onPress={()=>navigation.navigate('TextInput')}
+    />
+  </View>
+  )
 };
 
-HomeScreen.navigationOptions = {
-  title: 'Home'
-};
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30
+  }
+});
 
 export default HomeScreen;
